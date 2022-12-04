608a4a4a2593333d9e50254063cce0d38f8f5f13
Kajalgupta1011 <guptakajal040917@gmail.com>
2022-12-04 18:49:20 +0530

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,39 @@
-import Head from "next/head";
-import Header from "../Components/Header";
+// Get https://fakestoreapi.com/products
 
-export default function Home() {
+import Head from "next/head";
+import Banner from "../Components/Banner";
+import Header from "../Components/Header";
+import ProductFeed from "../Components/ProductFeed";
+
+export default function Home({products}) {
   return (
-    <div>
+    <div className="bg-gray-100 ">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
-
+      {/* Hader Compo */}
       <Header/>
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner/>
+
+        {/* ProductFeed */}
+        <ProductFeed products={products}/>
+        
+      </main>
+
     </div>
   );
 }
+
+export async function getServerSideProps(context){
+  const products  = await fetch('https://fakestoreapi.com/products').then((res)=>res.json()
+  );
+  return {
+    props:{
+      products,
+    }
+  }
+}
+
