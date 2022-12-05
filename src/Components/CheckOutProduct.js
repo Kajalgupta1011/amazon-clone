3744a6fd6862c65cf104ab0b9b8c72de7c86034b
Kajalgupta1011 <guptakajal040917@gmail.com>
2022-12-05 14:50:25 +0530

    code update

--- src/Components/CheckOutProduct.js
+++ src/Components/CheckOutProduct.js
@@ -1,18 +1,28 @@
-import Image from 'next/image'
+import StarRateIcon from '@mui/icons-material/StarRate';
+import Image from 'next/legacy/image'
 import React from 'react'
 
 function CheckOutProduct({ id, title, price, rating, description, category, image, hasPrime }) {
   return (
     <div className='grid grid-cols-5'>
+        Hello
         <Image
             src={image}
-            alt='image'
             height={200}
             width={200}
-            objectFit='contain'
+            objectFit="contain"
         />
+        {/* middle */}
+        <div className="col-span-3 mx-5">
+            <p>{title}</p>
+            <div>
+                {Array(rating).fill().map((_,i)=>(
+                    <StarRateIcon key={i} className='h-5 text-yellow-500'/>
+                ))}
+            </div>
+        </div>
     </div>
-  )
+  );
 }
 
 export default CheckOutProduct
